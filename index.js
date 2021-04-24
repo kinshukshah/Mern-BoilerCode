8e9c5fd37aa37fdbba6fabbd22608af936a3900b
kinshukshah <kinshukshah@gmail.com>
2021-04-24 23:37:35 +0530

    Move private information to dev.js file

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./model/user");
+const { mongoURI } = require("./config/key");
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
 mongoose
-  .connect(
-    "mongodb+srv://kshah99:maWhvrrAN7ycvdIF@neog-cluster.7hxhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  )
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then(() => console.log("DB connected"))
   .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello k$hah");
+});
+
+app.post("/api/users/register", async (req, res) => {
+  try {
+    const NewUser = new User(req.body);
+    const savedUser = await NewUser.save();
+    res.status(200).json({ success: true, userAdded: savedUser });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Unable to Add User", error: err });
+  }
 });
 
 app.listen(port, () => {
--- model/user.js
+++ model/user.js
@@ -31,6 +31,6 @@ const UserSchema = new Schema({
   },
 });
 
-const User=mongoose.model('User',UserSchema);
+const User = mongoose.model("User", UserSchema);
 
-module.exports={User};
+module.exports = { User };
